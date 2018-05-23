66af28a77c60c0e1192dc9f1ce6be8567c76bf1a
Sungkyun Chang <1ronyar@gmail.com>
2018-05-24 04:20:34 +0900

    aa

---
+++ collect-hive-tables.py
@@ -0,0 +1,28 @@
+import subprocess # required for executing command line
+from shlex import split
+
+# File path
+HIVE_BIN_PATH = '/home/hadoop/hive/bin/hive'
+#HADOOP_BIN_PATH = '/home/hadoop/hadoop-2.6.0-cdh5.5.1/bin/hadoop'
+OUTPUT_CSV_DIR = '/home/1ronyar/melon-dataset/hive-tables/'
+OUTPUT_MP3_DIR = '/home/1ronyar/melon-dataset/audio/'
+
+
+# Get a list of table names
+command = split(HIVE_BIN_PATH + " -e 'show tables'")
+result = subprocess.check_output(command)
+table_names = result.decode("utf-8").split()
+print('Found {} tables from hive.'.format(len(table_names)))
+
+# Get tables as .csv
+for tname in table_names:
+    command = split(HIVE_BIN_PATH + " -e 'select * from " + tname + "' | sed 's/[\t]/,/g' > " + OUTPUT_CSV_DIR + tname + ".csv" )
+    subprocess.call(command)
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+ ## 멜론 서버에서 hive, hadoop 인터페이스를 통해 meta data table과 audio file을 저장.
+
