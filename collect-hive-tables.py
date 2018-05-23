019092a27656f0a5b7ae677601d423713c710902
Sungkyun Chang <1ronyar@gmail.com>
2018-05-24 08:19:15 +0900

    added copy-hadoop-audio.py

--- collect-hive-tables.py
+++ collect-hive-tables.py
@@ -4,14 +4,12 @@ from shlex import split
 
 # File path
 HIVE_BIN_PATH = '/home/hadoop/hive/bin/hive'
-#HADOOP_BIN_PATH = '/home/hadoop/hadoop-2.6.0-cdh5.5.1/bin/hadoop'
-OUTPUT_CSV_DIR = '/home/1ronyar/melon-dataset/hive-tables/'
-OUTPUT_MP3_DIR = '/home/1ronyar/melon-dataset/audio/'
+TMP_CSV_DIR = '/home/1ronyar/melon-dataset/hive-tables/'
+OUTPUT_CSV_DIR = 'marg-cb:/home/work/melon-dataset/hive-tables'
 
-if not os.path.exists(OUTPUT_CSV_DIR):
-    os.makedirs(OUTPUT_CSV_DIR)
-if not os.path.exists(OUTPUT_MP3_DIR):
-    os.makedirs(OUTPUT_MP3_DIR)
+
+if not os.path.exists(TMP_CSV_DIR):
+    os.makedirs(TMP_CSV_DIR)
 
 # Get a list of table names
 command = split(HIVE_BIN_PATH + " -e 'show tables'")
@@ -23,7 +21,15 @@ print('Found {} tables from hive.'.format(len(table_names)))
 table_names = [table_names[5], table_names[6], table_names[7], table_names[14]]
 # Get tables as .csv
 for tname in table_names:
-    os.system(HIVE_BIN_PATH + " -e 'select * from " + tname + "' | sed 's/[\t]/,/g' > " + OUTPUT_CSV_DIR + tname + ".csv")
+    os.system(HIVE_BIN_PATH + " -e 'select * from " + tname + "' | sed 's/[\t]/,/g' > " + TMP_CSV_DIR + tname + ".csv")
+
+# Move files to docker volume
+command = "docker cp " + TMP_CSV_DIR + " " + OUTPUT_CSV_DIR
+assert(0==os.system(command)) # error if copying to docker volume was unsuccessful 
+
+# remove temporary files
+command = "rm -r " + TMP_CSV_DIR + "*"
+os.system(command)
 
 
 
---
+++ copy-hadoop-audio.py
@@ -0,0 +1,45 @@
+import subprocess # required for executing command line
+import os
+from shlex import split
+
+# File path
+HADOOP_BIN_PATH = '/home/hadoop/hadoop-2.6.0-cdh5.5.1/bin/hadoop'
+TMP_MP3_DIR = '/home/1ronyar/melon-dataset/audio/'  # slash(/) in the last is necessary!
+OUTPUT_MP3_DIR = 'marg-cb:/home/work/melon-dataset/audio'
+
+if not os.path.exists(TMP_MP3_DIR):
+    os.makedirs(TMP_MP3_DIR)
+
+# Get a list of directories in media_files directory 
+command = split(HADOOP_BIN_PATH + " fs -ls /media_files/")
+result = subprocess.check_output(command)
+result = result.decode("utf-8").split()
+dir_all = [s for s in result if '/media_files/' in s]
+total_dir = len(dir_all)
+print('Found {} sub-directories in media_files directory.'.format(total_dir))
+
+# Copy
+cnt = 1
+for dir in dir_all:
+    dir_source = dir
+    dir_target = OUTPUT_MP3_DIR
+    command = HADOOP_BIN_PATH + " fs -copyToLocal " + dir_source + " " + dir_target 
+    assert(0==os.system(command)) # error if 'hadoop fs -copyToLocal' command was unsuccessful
+    os.system("du -bsh " + TMP_MP3_DIR + "*")
+
+    # move to docker volume
+    command = "docker cp " + TMP_MP3_DIR + " " + OUTPUT_MP3_DIR
+    assert(0==os.system(command)) # error if copying to docker volume was unsuccessful
+
+    # remove temporary files
+    command = "rm -r " + TMP_MP3_DIR + "*"
+    os.system(command)
+
+    # print progresss
+    print('copy-hadoop-audio.py:{}/{} completed'.format(cnt, total_dir))
+    cnt += 1
+
+
+
+
+
